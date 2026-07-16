bb8b3049ef9864e4fb2f2f1842b62416538ac3c9
parham-bakht <parhambakhtiari1380@gmail.com>
2026-07-16 14:10:58 +0330

    forloop part1

---
+++ forloop.py
@@ -0,0 +1,6 @@
+# ctrl + j = terminal
+# run = python <filename.py>
+for i in range(3):
+    print(i)
+    print("Hello World")
+    
---
+++ forloop2.py
@@ -0,0 +1,5 @@
+
+students = ["kiana","aryan","amirreza","nil"]
+for i in students:
+    print(i)
+    
