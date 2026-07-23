40884d6fe83d085f7d7ee80b0dfd8fa2cf376281
parham-bakht <parhambakhtiari1380@gmail.com>
2026-07-23 14:09:00 +0330

    sdad

--- dict.py
+++ dict.py
@@ -1,6 +1,10 @@
 
 cars = {"benz":"c200","bmw":"i8","peugeot":"405"}
-students = {"aryan":"bandar abbas","nil":"tehran","amirreza":"shiraz"}
 
-print(students.get("aryan"))
-print(students["aryan"])
+students = {"aryan":"tabriz","nil":"tehran","amirreza":"shiraz"}
+
+# print(students.get("aryan"))
+# print(students["aryan"])
+students.update({"kiana":"tehran"})
+students.update({"aryan":"bandar abbas"})
+print(students)
